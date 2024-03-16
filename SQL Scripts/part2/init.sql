9283c4c9ab96d2ae7ef928b6ae07c16ad2f067fb
megy <dominik000@pobox.sk>
2024-03-16 14:03:57 +0100

    SQL- vytvorenie tabuliek

--- SQL Scripts/part2/init.sql
+++ SQL Scripts/part2/init.sql
@@ -1,64 +1,136 @@
 /*Tento scrpit blah blah blah*/
 
+/*Tabulky z ER diagramu*/
 CREATE TABLE Dealer (
-    id INT,
-    name VARCHAR(64),
-    surname VARCHAR(64),
-    birthDate DATE
+    dealer_id INT not null,
+    name VARCHAR(64) not null,
+    surname VARCHAR(64) not null,
+    birth_date DATE not null,
+    CONSTRAINT dealer_pk PRIMARY KEY (dealer_id)
 );
 
 CREATE TABLE Jailer (
-    id INT,
-    name VARCHAR(64),
-    surname VARCHAR(64),
-    birthDate DATE
+    jailer_id INT not null,
+    jail_id INT not null,
+    name VARCHAR(64) not null,
+    surname VARCHAR(64) not null,
+    birth_date DATE not null,
+    CONSTRAINT jailer_pk PRIMARY KEY (jailer_id)
 );
 
 CREATE TABLE Customer (
-    id INT,
-    name VARCHAR(64),
-    surname VARCHAR(64),
-    birthDate DATE,
-    cellNumber INT,
-    cellType VARCHAR(256) /*ENUM ?*/
+    customer_id INT not null,
+    jail_id INT not null,
+    name VARCHAR(64) not null,
+    surname VARCHAR(64) not null,
+    birth_date DATE not null,
+    cell_number INT,
+    cell_type VARCHAR(256),
+    CONSTRAINT customer_pk PRIMARY KEY (customer_id)
 );
 
-CREATE TABLE Order (
-    orderId INT,
-    price FLOAT(2),
-    deliveryType VARCHAR(256) /*ENUM ?*/
-    deliveryDate DATE
+CREATE TABLE Item_order (
+    order_id INT not null,
+    customer_id INT not null,
+    price FLOAT(2) not null,
+    delivery_type VARCHAR(256) not null,
+    delivery_date DATE,
+    CONSTRAINT order_pk PRIMARY KEY (order_id)
 );
 
 CREATE TABLE Pastry (
-    EAN INT,
-    weight INT,/*v gramoch*/
-    pastryType INT, /*ENUM :*/
+    EAN INT not null,
+    order_id INT not null,
+    weight INT not null,/*v gramoch*/
+    pastry_type VARCHAR(256) not null,
+    CONSTRAINT pastry_pk PRIMARY KEY (EAN)
 );
 
-/*TODO: oprav v ER z ingredients*/
 CREATE TABLE Ingredient (
-    id INT,
-    amount INT,
-    price FLOAT(2),
-    alergens STRING[]/*TODO:!!! možno ENUM ?*/
-    gluten BOOLEAN
+    ingredient_id INT not null,
+    amount INT not null,
+    price FLOAT(2) not null,
+    CONSTRAINT ingredient_pk PRIMARY KEY (ingredient_id)
+);
+
+CREATE TABLE Allergen (
+    allergen_name VARCHAR(32) not null,
+    CONSTRAINT allergen_pk PRIMARY KEY (allergen_name)
 );
 
 CREATE TABLE Jail (
-    id INT,
-    address VARCHAR(256)/*možno rozšíriť na novú tablu*/
+    jail_id INT not null,
+    delivery_zone_id INT not null,
+    address VARCHAR(256) not null,
+    CONSTRAINT jail_id PRIMARY KEY (jail_id)
 );
 
 CREATE TABLE Shift (
-    id INT,
-    startTime TIMESTAMP,
-    endTime TIMESTAMP
+    shift_id INT not null,
+    jail_id INT not null,
+    start_time TIMESTAMP not null,
+    end_time TIMESTAMP not null,
+    CONSTRAINT shift_pk PRIMARY KEY (shift_id)
 );
 
 CREATE TABLE Delivery_zone (
-    id INT
+    delivery_zone_id INT not null,
+    dealer_id INT not null,
+    CONSTRAINT delivery_zone_pk PRIMARY KEY (delivery_zone_id)
 );
 
-/*na id by sa možno dalo použiť ROWID * "You should not use ROWID as the primary key of a table" zdroj Google*/
-/*TODO: porieš INTY nesu dobre*/
+CREATE TABLE Backed_item (
+    backed_item_id INT not null,
+    weight INT not null,
+    CONSTRAINT backed_item_pk PRIMARY KEY (backed_item_id)
+);
+
+/*Pomocne tabulky pre MN relacie*/
+CREATE TABLE Dealer_Jailer (
+    dealer_id INT not null,
+    jailer_id INT not null
+);
+
+CREATE TABLE Jailer_Shift (
+    jailer_id INT not null,
+    shift_id INT not null
+);
+
+CREATE TABLE Backed_item_Pastry (
+    backed_item_id INT not null,
+    EAN INT not null
+);
+
+CREATE TABLE Ingredient_Pastry (
+    ingredient_id INT not null,
+    EAN INT not null
+);
+
+CREATE TABLE Ingredient_Allergern (
+    ingredient_id INT not null,
+    allergen_name VARCHAR(32) not null
+);
+
+/*Set foreign keys*/
+ALTER TABLE Jailer ADD CONSTRAINT Jjail_fk FOREIGN KEY (jail_id) REFERENCES Jail(jail_id);
+ALTER TABLE Customer ADD CONSTRAINT Cjail_fk FOREIGN KEY (jail_id) REFERENCES Jail(jail_id);
+ALTER TABLE Item_order ADD CONSTRAINT customer_fk FOREIGN KEY (customer_id) REFERENCES Customer(customer_id);
+ALTER TABLE Pastry ADD CONSTRAINT Porder_fk FOREIGN KEY (order_id) REFERENCES Item_order(order_id);
+ALTER TABLE Jail ADD CONSTRAINT delivery_zone_fk FOREIGN KEY (delivery_zone_id) REFERENCES Delivery_zone(delivery_zone_id);
+ALTER TABLE Shift ADD CONSTRAINT Sjail_fk FOREIGN KEY (jail_id) REFERENCES Jail(jail_id);
+ALTER TABLE Delivery_zone ADD CONSTRAINT dealer_fk FOREIGN KEY (dealer_id) REFERENCES Dealer(dealer_id);
+
+ALTER TABLE Dealer_Jailer ADD CONSTRAINT DJdealer_fk FOREIGN KEY (dealer_id) REFERENCES Dealer(dealer_id);
+ALTER TABLE Dealer_Jailer ADD CONSTRAINT DJjailer_fk FOREIGN KEY (jailer_id) REFERENCES Jailer(jailer_id);
+
+ALTER TABLE Jailer_Shift ADD CONSTRAINT JSjailer_fk FOREIGN KEY (jailer_id) REFERENCES Jailer(jailer_id);
+ALTER TABLE Jailer_Shift ADD CONSTRAINT JSshift_fk FOREIGN KEY (shift_id) REFERENCES Shift(shift_id);
+
+ALTER TABLE Backed_item_Pastry ADD CONSTRAINT BPbacked_item_fk FOREIGN KEY (backed_item_id) REFERENCES Backed_item(backed_item_id);
+ALTER TABLE Backed_item_Pastry ADD CONSTRAINT BPEAN_fk FOREIGN KEY (EAN) REFERENCES Pastry(EAN);
+
+ALTER TABLE Ingredient_Pastry ADD CONSTRAINT IPingredient_fk FOREIGN KEY (ingredient_id) REFERENCES Ingredient(ingredient_id);
+ALTER TABLE Ingredient_Pastry ADD CONSTRAINT IPEAN_fk FOREIGN KEY (EAN) REFERENCES Pastry(EAN);
+
+ALTER TABLE Ingredient_Allergern ADD CONSTRAINT IAingredient_fk FOREIGN KEY (ingredient_id) REFERENCES Ingredient(ingredient_id);
+ALTER TABLE Ingredient_Allergern ADD CONSTRAINT IAallergen_name_fk FOREIGN KEY (allergen_name) REFERENCES Allergen(allergen_name);
--- SQL Scripts/reset.sql
+++ SQL Scripts/reset.sql
@@ -1,9 +1,41 @@
+ALTER TABLE Jailer DROP CONSTRAINT Jjail_fk;
+ALTER TABLE Customer DROP CONSTRAINT Cjail_fk;
+ALTER TABLE Item_order DROP CONSTRAINT customer_fk;
+ALTER TABLE Pastry DROP CONSTRAINT Porder_fk;
+ALTER TABLE Jail DROP CONSTRAINT delivery_zone_fk;
+ALTER TABLE Shift DROP CONSTRAINT Sjail_fk;
+ALTER TABLE Delivery_zone DROP CONSTRAINT dealer_fk;
+
+ALTER TABLE Dealer_Jailer DROP CONSTRAINT DJdealer_fk;
+ALTER TABLE Dealer_Jailer DROP CONSTRAINT DJjailer_fk;
+
+ALTER TABLE Jailer_Shift DROP CONSTRAINT JSjailer_fk;
+ALTER TABLE Jailer_Shift DROP CONSTRAINT JSshift_fk;
+
+ALTER TABLE Backed_item_Pastry DROP CONSTRAINT BPbacked_item_fk;
+ALTER TABLE Backed_item_Pastry DROP CONSTRAINT BPEAN_fk;
+
+ALTER TABLE Ingredient_Pastry DROP CONSTRAINT IPingredient_fk;
+ALTER TABLE Ingredient_Pastry DROP CONSTRAINT IPEAN_fk;
+
+ALTER TABLE Ingredient_Allergern DROP CONSTRAINT IAingredient_fk;
+ALTER TABLE Ingredient_Allergern DROP CONSTRAINT IAallergen_name_fk;
+
+
 DROP TABLE Dealer;
 DROP TABLE Jailer;
 DROP TABLE Customer;
-DROP TABLE Order;
+DROP TABLE Item_order;
 DROP TABLE Pastry;
 DROP TABLE Ingredient;
 DROP TABLE Jail;
 DROP TABLE Shift;
 DROP TABLE Delivery_zone;
+DROP TABLE Allergen;
+DROP TABLE Backed_item;
+
+DROP TABLE Dealer_Jailer;
+DROP TABLE Jailer_Shift;
+DROP TABLE Backed_item_Pastry;
+DROP TABLE Ingredient_Pastry;
+DROP TABLE Ingredient_Allergern;
