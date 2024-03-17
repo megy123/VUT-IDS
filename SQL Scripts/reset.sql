4240c230a4f6a1e9a115da71d4639dba4027c313
megy <dominik000@pobox.sk>
2024-03-17 15:22:32 +0100

    2 faza megyho časť

--- SQL Scripts/reset.sql
+++ SQL Scripts/reset.sql
@@ -1,29 +1,43 @@
-ALTER TABLE Jailer DROP CONSTRAINT Jjail_fk;
-ALTER TABLE Customer DROP CONSTRAINT Cjail_fk;
-ALTER TABLE Item_order DROP CONSTRAINT customer_fk;
-ALTER TABLE Item_order DROP CONSTRAINT first_pastry_EAN;
-ALTER TABLE Pastry DROP CONSTRAINT Porder_fk;
-ALTER TABLE Jail DROP CONSTRAINT delivery_zone_fk;
-ALTER TABLE Jail DROP CONSTRAINT first_shift_fk;
-ALTER TABLE Shift DROP CONSTRAINT Sjail_fk;
-ALTER TABLE Delivery_zone DROP CONSTRAINT dealer_fk;
+--pri refraktorizacii sa mi to pojebalo
+ALTER TABLE Jailer DROP CONSTRAINT jailer_jail_fk;
+ALTER TABLE Customer DROP CONSTRAINT customer_jail_fk;
+ALTER TABLE Item_order DROP CONSTRAINT item_order_customer_fk;
+ALTER TABLE Pastry DROP CONSTRAINT pastry_order_fk;
+ALTER TABLE Jail DROP CONSTRAINT jail_delivery_zone_fk;
+ALTER TABLE Shift DROP CONSTRAINT shift_jail_fk;
+ALTER TABLE Delivery_zone DROP CONSTRAINT delivery_zone_dealer_fk;
 
-ALTER TABLE Dealer_Jailer DROP CONSTRAINT DJdealer_fk;
-ALTER TABLE Dealer_Jailer DROP CONSTRAINT DJjailer_fk;
+--at least one rels
+ALTER TABLE Item_order DROP CONSTRAINT item_order_first_pastry_ean;
+ALTER TABLE Jail DROP CONSTRAINT jail_first_shift_fk;
+ALTER TABLE Backed_item DROP CONSTRAINT backed_item_first_pastry;
+ALTER TABLE Shift DROP CONSTRAINT shift_first_jailer;
+ALTER TABLE Ingredient DROP CONSTRAINT ingredient_first_pastry;
+ALTER TABLE Pastry DROP CONSTRAINT pastry_first_ingredient;
 
-ALTER TABLE Jailer_Shift DROP CONSTRAINT JSjailer_fk;
-ALTER TABLE Jailer_Shift DROP CONSTRAINT JSshift_fk;
+ALTER TABLE Dealer_Jailer DROP CONSTRAINT dealer_jailer_dealer_fk;
+ALTER TABLE Dealer_Jailer DROP CONSTRAINT dealer_jailer_jailer_fk;
 
-ALTER TABLE Backed_item_Pastry DROP CONSTRAINT BPbacked_item_fk;
-ALTER TABLE Backed_item_Pastry DROP CONSTRAINT BPEAN_fk;
+ALTER TABLE Jailer_Shift DROP CONSTRAINT jailer_shift_jailer_fk;
+ALTER TABLE Jailer_Shift DROP CONSTRAINT jailer_shift_shift_fk;
 
-ALTER TABLE Ingredient_Pastry DROP CONSTRAINT IPingredient_fk;
-ALTER TABLE Ingredient_Pastry DROP CONSTRAINT IPEAN_fk;
+ALTER TABLE Backed_item_Pastry DROP CONSTRAINT backed_item_pastry_backed_item_fk;
+ALTER TABLE Backed_item_Pastry DROP CONSTRAINT backed_item_pastry_ean_fk;
 
-ALTER TABLE Ingredient_Allergern DROP CONSTRAINT IAingredient_fk;
-ALTER TABLE Ingredient_Allergern DROP CONSTRAINT IAallergen_name_fk;
+ALTER TABLE Ingredient_Pastry DROP CONSTRAINT ingredient_pastry_ingredient_fk;
+ALTER TABLE Ingredient_Pastry DROP CONSTRAINT ingredient_pastry_ean_fk;
 
+ALTER TABLE Ingredient_Allergen DROP CONSTRAINT ingredient_allergen_ingredient_fk;
+ALTER TABLE Ingredient_Allergen DROP CONSTRAINT ingredient_allergen_allergen_name_fk;
 
+ALTER TABLE Item_order DROP CONSTRAINT item_price_validity;
+ALTER TABLE Pastry DROP CONSTRAINT pastry_weight_validity;
+ALTER TABLE Ingredient DROP CONSTRAINT ingredient_price_validity;
+ALTER TABLE Ingredient DROP CONSTRAINT ingredient_amount_validity;
+ALTER TABLE Backed_item DROP CONSTRAINT backed_item_weight_validity;
+ALTER TABLE Shift DROP CONSTRAINT shift_time_validity;
+
+--table drop
 DROP TABLE Dealer;
 DROP TABLE Jailer;
 DROP TABLE Customer;
@@ -40,4 +54,4 @@ DROP TABLE Dealer_Jailer;
 DROP TABLE Jailer_Shift;
 DROP TABLE Backed_item_Pastry;
 DROP TABLE Ingredient_Pastry;
-DROP TABLE Ingredient_Allergern;
+DROP TABLE Ingredient_Allergen;
