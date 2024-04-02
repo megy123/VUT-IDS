fcad242445e94f8078182f59b9bce9e2e005a4bd
Daniela Sándorová <115703499+setimokit@users.noreply.github.com>
2024-04-02 20:18:18 +0200

    FIX: part 2 now actually works

--- SQL Scripts/reset.sql
+++ SQL Scripts/reset.sql
@@ -1,5 +1,30 @@
 
 --table drop
+/*DROP TABLE Backed_item_Pastry;
+DROP TABLE Ingredient_Pastry;
+DROP TABLE Dealer_Jailer;
+DROP TABLE Jailer_Shift;
+DROP TABLE Ingredient_Allergen;
+
+DROP TABLE Backed_item;
+
+DROP TABLE Shift;
+DROP TABLE Jailer;
+
+DROP TABLE Allergen;
+DROP TABLE Ingredient;
+
+DROP TABLE Dealer;
+
+DROP TABLE Delivery_zone;
+
+
+DROP TABLE Jail;
+DROP TABLE Customer;
+DROP TABLE Item_order;
+DROP TABLE Pastry;*/
+
+
 DROP TABLE Backed_item_Pastry;
 DROP TABLE Ingredient_Pastry;
 DROP TABLE Dealer_Jailer;
@@ -22,24 +47,5 @@ DROP TABLE Delivery_zone;
 DROP TABLE Dealer;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-DROP TABLE Backed_item;
 
 
